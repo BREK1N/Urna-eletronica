f72066ee47f5a60cc9d0ed3b3f73c5f235c7396b
Lucas Ferreira <lokoporgame194@gmail.com>
2022-11-12 17:11:32 -0300

    urna completa
    sistema da urna completa

--- app/controllers/defaults.py
+++ app/controllers/defaults.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template,request, redirect, url_for, session
 from app import app
 
+andre = []
+lukas = []
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    urna = request.form.get('urna')
-    return render_template('index.html', urna=urna)
+    return render_template('index.html')
 
+@app.route('/votacao', methods=['GET', 'POST'])
+def votacao():
+
+    global andre
+    global lukas
+
+    urna = request.form.get('urna')
+    if urna == "76":
+        andre.append(1)
+    if urna == "12":
+        lukas.append(1)
+
+    return render_template('votacao.html', urna=urna)
+
+@app.route('/resultado')
+def resultado():
+    return render_template('resultado.html', lukas=len(lukas), andre=len(andre))
 
